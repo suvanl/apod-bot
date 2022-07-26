b65a0433d7b27201f660089453c767d4c253c651
suvanl <32813609+suvanl@users.noreply.github.com>
2022-07-26 16:22:57 +0100

    fetchMediaData: update comments

--- src/tools/common/fetch.ts
+++ src/tools/common/fetch.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import { APODResponse } from "../../types";
 
 export const fetchMediaData = async (): Promise<APODResponse> => {
-    // Fetch image data from the NASA APOD API
+    // Fetch media data from the NASA APOD API
     const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
     const img = await axios.get<APODResponse>(url);
 
-    // Return the JSON object containing data such as the author, description and image URL
+    // Return the JSON object containing data such as the author, description and media URL
     return img.data;
 };
