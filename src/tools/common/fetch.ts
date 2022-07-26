b93bc1d666625ca09a580811476ee84fdd0949ec
suvanl <32813609+suvanl@users.noreply.github.com>
2022-07-26 17:59:29 +0100

    fetchMediaData: use destructuring

--- src/tools/common/fetch.ts
+++ src/tools/common/fetch.ts
@@ -4,8 +4,8 @@ import { APODResponse } from "../../types";
 export const fetchMediaData = async (): Promise<APODResponse> => {
     // Fetch media data from the NASA APOD API
     const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
-    const img = await axios.get<APODResponse>(url);
+    const { data } = await axios.get<APODResponse>(url);
 
     // Return the JSON object containing data such as the author, description and media URL
-    return img.data;
+    return data;
 };
