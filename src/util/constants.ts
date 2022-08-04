eda2c9ffad6fd9b63c615cb100df58a162dd58f0
suvanl <32813609+suvanl@users.noreply.github.com>
2022-08-04 13:13:31 +0100

    Create `TWEET_MAX_LENGTH` constant

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,3 +1,4 @@
 export const IMAGE_MAX_SIZE = 5242880;    // 5MB
 export const GIF_MAX_SIZE   = 15728640;   // 15MB
 export const VIDEO_MAX_SIZE = 536870912;  // 512MB
+export const TWEET_MAX_LENGTH = 280;      // 280 chars
