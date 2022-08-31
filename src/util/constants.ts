f64dacb38732d7ddc5de721798f4db4233cd77c8
suvanl <32813609+suvanl@users.noreply.github.com>
2022-09-01 00:08:21 +0100

    Define IG min/max aspect ratio values

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,3 +1,5 @@
+import { AspectRatio } from "../types";
+
 // Twitter
 export const IMAGE_MAX_SIZE = 5242880;    // 5MB
 export const GIF_MAX_SIZE   = 15728640;   // 15MB
@@ -7,3 +9,5 @@ export const TWEET_MAX_LENGTH = 280;      // 280 chars
 
 // Instagram
 export const IG_CAPTION_MAX_LENGTH = 2200;  // 2200 characters
+export const IG_IMAGE_ASPECT_RATIO_MIN: AspectRatio = { width: 4, height: 5 };
+export const IG_IMAGE_ASPECT_RATIO_MAX: AspectRatio = { width: 1.91, height: 1 };
