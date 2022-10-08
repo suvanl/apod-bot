ca97299f7964bd9b758dbfe7bbf54cb425d06d07
suvanl <32813609+suvanl@users.noreply.github.com>
2022-10-08 23:16:23 +0100

    Remove hardcoded date in fetch URL

--- src/tools/common/fetch.ts
+++ src/tools/common/fetch.ts
@@ -4,7 +4,7 @@ import { APODResponse } from "../../types";
 
 export const fetchMediaData = async (): Promise<APODResponse> => {
     // Fetch media data from the NASA APOD API
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=2022-10-02`;
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
     const { data } = await axios.get<APODResponse>(url);
 
     // Return the JSON object containing data such as the author, description and media URL
